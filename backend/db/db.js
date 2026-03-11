403e819fe806c0eb51acfc4a0984cbcf17f72df8
Ali Wajdan <aliwajdan.it@gmail.com>
2026-03-11 05:41:58 +0500

    commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,4 +1,4 @@
-import pkg from 'pg';
+import pkg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -6,8 +6,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-})
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 export const query = (text, params) => pool.query(text, params);
-
-
